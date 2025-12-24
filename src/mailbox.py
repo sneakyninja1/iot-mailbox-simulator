db015a7711349914299d9e8e7a6f221902196c3c
Taha Iqbal <tahaiqbal542@gmail.com>
2025-12-24 09:22:12 -0600

    Implemented sample light level

--- src/mailbox.py
+++ src/mailbox.py
@@ -1,12 +1,33 @@
-class MailboxSensor: 
-    def __init__(self): 
-        self.mail_present = False; 
-    
-    def detect_mail(self): 
-        self.mail_present = True; 
-    
-    def clear_mail(self): 
-        self.mail_present = False; 
+import time 
+import random 
+import threading
+from typing import Callable, Optional
 
-    def status(self): 
-        return self.mail_present; 
+class IOTMailbox: 
+    def __intit__(self, interval_ms, signal_callback):
+        self._interval_ms = interval_ms
+        self._signal_callback = signal_callback
+        self._running = False
+        self._thread: Optional[threading.Thread] = None
+        self._last_light_level = 1.0
+
+
+    def start(self): 
+        pass
+    
+    def stop(self): 
+        pass
+
+    def _monitor_loop(self):
+        pass
+
+    def _sample_light_level(self): 
+        magnitude = random.random()
+
+        if (self._sample_light_level >= 0):
+            new_level = -magnitude
+        else:
+            new_level = magnitude
+        
+        self._sample_light_level = new_level
+        return new_level
