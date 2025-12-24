20d561a8648e78ec2739dec977a6c553dc752e28
Taha Iqbal <tahaiqbal542@gmail.com>
2025-12-24 10:09:20 -0600

    Star, stop, and monitor loop methods are now implemented

--- src/mailbox.py
+++ src/mailbox.py
@@ -13,13 +13,24 @@ class IOTMailbox:
 
 
     def start(self): 
-        pass
-    
+        if self._running: 
+            return
+        
+        self._running = True
+        self._thread= threading.Thread(target=self._monitor_loop, daemon = True)
+        self._thread.start()
+
     def stop(self): 
-        pass
+        if self._running == False:
+            return 
+    
+        self._running = False
 
     def _monitor_loop(self):
-        pass
+        while self._running:
+            light_level = self._sample_light_level()
+            self._signal_callback(light_level)
+            time.sleep(self._interval_ms / 1000)
 
     def _sample_light_level(self): 
         magnitude = random.random()
